9da362b3c316fd67393f0408a0d2395080cd147a
EkaBachrudin <vvalentine825@gmail.com>
2023-01-10 17:15:51 +0700

    Role Guard (Not Fixed, still editable from localstorage borwser)

--- src/app/shared/auth/role.guard.ts
+++ src/app/shared/auth/role.guard.ts
@@ -5,9 +5,10 @@ import {
   RouterStateSnapshot,
   Router,
 } from '@angular/router';
-import { async, map, Observable } from 'rxjs';
+import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 import { RoleService } from './role.service';
+import { Location } from '@angular/common';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
@@ -15,19 +16,22 @@ export class RoleGuard implements CanActivate {
   constructor(
     private authService: AuthService,
     private roleService: RoleService,
-    private router: Router
+    private router: Router,
+    private _location: Location
   ) {}
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    this.authService.rolesUser().forEach((data: any) => {
-      this.roles = data;
-      console.log(this.roles);
-    });
+    this.roles = JSON.parse(localStorage.getItem('roles') || '{}');
+    if (this.roles[0].name == next.data['role']) {
+      return true;
+    }
+    if (this.roles[0].name == next.data['role']) {
+      return true;
+    }
 
-    console.log(this.roles);
-
-    return true;
+    this._location.back();
+    return false;
   }
 }
